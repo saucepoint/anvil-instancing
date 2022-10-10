974c158f98469d29cbb3b181ec3ff42b2fedcf60
saucepoint <saucepoint@protonmail.com>
2022-10-10 15:54:00 -0400

    emit an event to test anvil state restoration

--- src/Counter.sol
+++ src/Counter.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.13;
 contract Counter {
     uint256 public number;
 
+    event SetNumber(address sender, uint256 newNumber);
+
     function setNumber(uint256 newNumber) public {
         number = newNumber;
+        emit SetNumber(msg.sender, newNumber);
     }
 
     function increment() public {
